fa860e7fa7501246b9534d87d09f293dad1c119d
evgenii-shvetsov <shvetsovea@gmail.com>
2022-05-09 15:50:25 -0700

    create cities array for mapping

---
+++ src/options.js
@@ -0,0 +1,23 @@
+const options =[
+    { lable:'Select an Option', value:'' },
+    { lable:'Alameda', value:'Alameda' },
+    { lable:'Contra Costa', value:'Contra Costa' },
+    { lable:'Los Angeles', value:'Los Angeles' },
+    { lable:'Marin', value:'Marin' },
+    { lable:'Monterey', value:'Monterey' },
+    { lable:'Napa', value:'Napa' },
+    { lable:'Orange', value:'Orange' },
+    { lable:'San Diego', value:'San Diego' },
+    { lable:'San Francisco', value:'San Francisco' },
+    { lable:'San Luis Obispo', value:'San Luis Obispo' },
+    { lable:'San Mateo', value:'San Mateo' },
+    { lable:'Santa Barbara', value:'Santa Barbara' },
+    { lable:'Santa Clara', value:'Santa Clara' },
+    { lable:'Santa Cruz', value:'Santa Cruz' },
+    { lable:'Sonoma', value:'Sonoma' },
+    { lable:'Ventura', value:'Ventura' },
+    { lable:'Other', value:'Other' },
+
+]
+
+export default options;
